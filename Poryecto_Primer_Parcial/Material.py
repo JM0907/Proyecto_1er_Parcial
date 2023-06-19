383a3bb44c3e161aa6a98d2d32603714e468c509
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-06-19 16:10:26 -0600

    Update Material.py

--- Poryecto_Primer_Parcial/Material.py
+++ Poryecto_Primer_Parcial/Material.py
@@ -71,42 +71,6 @@ class Material:
         self._cantidad_disponible = value
 
 
-class Libro(Material):
-    def __init__(self, codigo, autor, titulo, anio, editorial, disponible, cantidad_disponible, tipo_pasta):
-        super().__init__(codigo, autor, titulo, anio, editorial, disponible, cantidad_disponible)
-        self.id = Material.contador_libro + 1
-        self._tipo_pasta = tipo_pasta
-        Material.contador_libro += 1
-
-    @property
-    def tipo_pasta(self):
-        return self._tipo_pasta
-
-    @tipo_pasta.setter
-    def tipo_pasta(self, value):
-        self._tipo_pasta = value
-
-    def get_titulo(self):
-        return self._titulo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
